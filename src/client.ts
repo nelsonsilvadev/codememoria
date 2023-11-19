58dd3fa50efb16fbe2deb654f2e6f604bf12cde4
Nelson Silva <nelsonsilvadev@gmail.com>
2023-11-19 17:09:03 +0000

    Update client (local storage) and query (fragment)

--- src/client.ts
+++ src/client.ts
@@ -1,14 +1,32 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+// Note: Added apollo-link-context to set the authorization header each time a request is made.
+// Before that, if I manually set the token in localStorage, I had to refresh the page to see the data.
+import { setContext } from '@apollo/client/link/context'
+
+const GITHUB_GRAPHQL_API_URI = 'https://api.github.com/graphql'
 
 const httpLink = new HttpLink({
-  uri: 'https://api.github.com/graphql',
-  headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
-  },
+  uri: GITHUB_GRAPHQL_API_URI,
+})
+
+const authLink = setContext((_, { headers }) => {
+  // Note: If there's no token in localStorage, it will look for the token in the .env file.
+  const token =
+    localStorage.getItem('token') || process.env.REACT_APP_GITHUB_ACCESS_TOKEN
+  const authHeader = token ? `Bearer ${token}` : ''
+
+  return {
+    headers: {
+      ...headers,
+      authorization: authHeader,
+    },
+  }
 })
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
+  // Note: Couldn't forget the cache since it's a requirement for Apollo Client.
+  // I still need to learn more about cache policies and how to use them.
   cache: new InMemoryCache(),
 })
 
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,27 +1,13 @@
 import { gql } from '@apollo/client'
 
+// Note: Just added a GraphQL fragment to avoid repeating the same fields in the queries.
+// Something that will not happen since I'm just using one query in this app.
 export const SEARCH_REPOSITORIES = gql`
   query SearchRepositories($query: String!, $after: String) {
     search(query: $query, type: REPOSITORY, first: 10, after: $after) {
       edges {
         node {
-          ... on Repository {
-            id
-            name
-            description
-            languages(first: 10) {
-              edges {
-                node {
-                  id
-                  name
-                }
-              }
-            }
-            stargazerCount
-            forkCount
-            url
-            updatedAt
-          }
+          ...RepositoryFields
         }
       }
       pageInfo {
@@ -29,5 +15,23 @@ export const SEARCH_REPOSITORIES = gql`
         hasNextPage
       }
     }
+  }
+
+  fragment RepositoryFields on Repository {
+    id
+    name
+    description
+    languages(first: 10) {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+    stargazerCount
+    forkCount
+    url
+    updatedAt
   }
 `
